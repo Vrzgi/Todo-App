89b8874a7fab5ccef025e94ff142f618e029de11
Hamid Reza Vorezgi Nejad <Vorezgi@gmail.com>
2023-05-13 17:44:49 +0430

    todo Toggeling the Form

--- Assets/JS/todos.js
+++ Assets/JS/todos.js
@@ -1,23 +1,6 @@
 //Check off Specific To-Do by Clicking
 $("ul").on("click","li",function(){
-    $(this).toggleClass("complated");
-    /*
-    //if <li> is grey
-    if($(this).css("color") === "rgb(128, 128, 128)"){
-        //turn it black
-        $(this).css(({
-            color : "black",
-            textDecoration : "none"
-        }));
-    }
-    else{
-        //turn it grey
-        $(this).css(({
-            color: "grey",
-            textDecoration: "line-through"
-        }));
-    }
-    */
+    $(this).toggleClass("Complated!");
 });
 //Click on X to delete To-Do
 $("ul").on("click","span",function(event){
@@ -27,14 +10,15 @@ $("ul").on("click","span",function(event){
     event.stopPropagation();
 });
 $("input[type='text'").keypress(function(event){
-    //console.log("Key Pressed !")
     //check enter
     if(event.which === 13){
-        //console.log("You Press ENTER !");
         //grabbing new todo text from input
         var todoText = $(this).val();
         $(this).val(""); //empty after each add
         //create a new li and add to ul
         $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +"</li>");
     }
+});
+$(".fa-plus").click(function(){
+    $("input[type='text'").fadeToggle();
 });
